d0cafa59c498bbe795923f2304ad4b6e11b21745
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2024-01-29 19:31:13 +0530

    push front-end part

---
+++ Form/Front-end/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import axois from 'axios'
+import Form from './compontes/Form'
+function App() {
+  // let[data , setData]= useState()
+  //  useEffect()
+  // axois.post("", {})
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
